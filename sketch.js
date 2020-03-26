cc8419668f3815664f0445d7e1d6c9bee7eeabaa
Anurag <anuragsai.v@gmail.com>
2020-03-26 17:03:20 +0530

    log6

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +37,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log6 = new Log(400,390,70,PI/2);
+
 }
 
 function draw(){
@@ -61,4 +64,6 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
 }
